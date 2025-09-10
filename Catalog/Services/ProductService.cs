3a3a77e4f3d571ca08ee1a66e69093068f810e35
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-09-11 08:15:31 +1200

    Publish ProductPriceChanged integration event in ProductService and add message bus dependency

--- Catalog/Services/ProductService.cs
+++ Catalog/Services/ProductService.cs
@@ -1,10 +1,12 @@
 ﻿using Catalog.Data;
 using Catalog.Models;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+using ServiceDefaults.Messaging.Events;
 
 namespace Catalog.Services;
 
-public class ProductService(ProductDbContext dbContext)
+public class ProductService(ProductDbContext dbContext, IBus bus)
 {
     public async Task<IEnumerable<Product>> GetProductsAsync()
     {
@@ -24,6 +26,25 @@ public class ProductService(ProductDbContext dbContext)
 
     public async Task UpdateProductAsync(Product updatedProduct, Product inputProduct)
     {
+      
+        // TODO: Dual write problem
+        // Outbox pattern to be added as an enhancement
+        
+        // if price has changed, raise ProductPriceChanged integration event
+        if (updatedProduct.Price != inputProduct.Price)
+        {
+            // Publish product price changed integration event for update basket prices
+            var integrationEvent = new ProductPriceChangedIntegrationEvent
+            {
+                ProductId = updatedProduct.Id, // Id only comes from db entity
+                Name = inputProduct.Name,
+                Description = inputProduct.Description,
+                Price = inputProduct.Price, //set updated product price
+                ImageUrl = inputProduct.ImageUrl
+            };
+            await bus.Publish(integrationEvent);
+        }
+        
         // update product with new values
         updatedProduct.Name = inputProduct.Name;
         updatedProduct.Description = inputProduct.Description;
