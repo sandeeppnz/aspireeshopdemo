282a562dff447419c1a68a0fe3a6f309b86eae40
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-09-10 19:26:33 +1200

    Fetch product details from Catalog API before updating basket items.

--- Basket/Services/BasketService.cs
+++ Basket/Services/BasketService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text.Json;
+using Basket.ApiClients;
 using Basket.Models;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Basket.Services;
 
-public class BasketService(IDistributedCache cache)
+public class BasketService(IDistributedCache cache, CatalogApiClient catalogApiClient)
 {
     public async Task<ShoppingCart?> GetBasket(string userName)
     {
@@ -14,6 +15,15 @@ public class BasketService(IDistributedCache cache)
     }
     public async Task UpdateBasket(ShoppingCart basket)
     {
+        // Before update(Add/remove Item) into SC, we should call Catalog ms GetProductById method
+        // Get latest product information and set Price and ProductName when adding item into SC
+        foreach (var item in basket.Items)
+        {
+            var product = await catalogApiClient.GetProductById(item.ProductId);
+            item.Price = product.Price;
+            item.ProductName = product.Name;
+        }
+
         await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
     }
     public async Task DeleteBasket(string userName)
